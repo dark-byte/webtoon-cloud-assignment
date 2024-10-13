31c34efc7e71294ddfaa5f246e59cdd27c1a10b4
Adrish Mitra <adrishmitra710@gmail.com>
2024-10-13 21:53:03 +0530

    serve index.html at /

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,11 @@ const manhwaData = require('./manhwa_data.json');
 
 app.use(express.static('public'));
 
+// Serve index.html at the root path
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 app.get('/api/manhwa', (req, res) => {
   res.json(manhwaData);
 });
