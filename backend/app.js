53e0f4861cfa9f63e65620a3abb2ba897386bb28
Adrish Mitra <adrishmitra710@gmail.com>
2024-10-13 21:56:09 +0530

    path

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
 const manhwaData = require('./manhwa_data.json');
@@ -8,7 +9,7 @@ app.use(express.static('public'));
 
 // Serve index.html at the root path
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 app.get('/api/manhwa', (req, res) => {
